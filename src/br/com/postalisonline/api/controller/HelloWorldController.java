fca2f5ab0740f37ca4f431f93ae7e69e420b6065
alessandro.lima <alessandro.lima@4quality.com.br>
2019-05-07 16:33:34 -0300

    Atualizando projeto

--- src/br/com/postalisonline/api/controller/HelloWorldController.java
+++ src/br/com/postalisonline/api/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package br.com.postalisonline.api.controller;
 
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,7 @@ public class HelloWorldController {
 	}
 	
 	@RequestMapping("/api/message")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message message() {
  
 		return new Message("Hello World !!!!") ;
